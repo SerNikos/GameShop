55aa1af33857d4bb703ff842e6ad3838111a9140
zeref64 <zbillks@gmail.com>
2023-01-07 15:00:12 +0200

    added api folder that contains backend

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function showAllUsers()
+    {
+        return response()->json(User::all());
+    }
+
+    public function showOneUser($id)
+    {
+        return response()->json(User::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+
+            'username' => 'required',
+            'password' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'location' => 'required|alpha',
+            'email' => 'required|email|unique:users'
+        ]);
+
+        $user = User::create($request->all());
+
+        return response()->json($user, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return response()->json($user, 200);
+    }
+
+    public function delete($id)
+    {
+        User::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
